e90efa8aa5222e308112bfcfa27a3db011fe0cd1
c21adano <c21adano@student.his.se>
2022-05-06 16:06:48 +0200

    Changed mountain_list.xml layout Wrote kod in Mountain.java

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,4 +1,39 @@
 package com.example.networking;
 
 public class Mountain {
+
+    private String mId;
+    private String mName;
+    private String mLocation;
+    private int mSize;
+    private int   mCost;
+
+
+    public Mountain(String id, String name, String location, int size, int cost) {
+        mId = id;
+        mName = name;
+        mLocation = location;
+        mSize = size;
+        mCost = cost;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public int getSize() {
+        return mSize;
+    }
+
+    public int getCost() {
+        return mCost;
+    }
 }
